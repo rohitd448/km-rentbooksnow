1a2d30d76bf81f889bf093196a54de3ad0fb6b8e
rohitd448 <rohitd448@gmail.com>
2020-07-22 20:40:28 +0530

    changed PDO

--- pdo_connection.php
+++ pdo_connection.php
@@ -1,5 +1,14 @@
 <?php
-echo 'check for pdo connection';
-$DB=new PDO("mysql:host=localhost;dbname=dbMyname",'root', 'root');
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 ?>
